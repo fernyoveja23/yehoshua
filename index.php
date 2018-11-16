37c12dc2ba1ca9676f322bbc46592b07c3de4869
fernyoveja23 <ferninand_g@hotmail.com>
2018-11-16 00:40:28 -0600

    Paginas de inicio

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>My first PHP Website</title>
+    </head>
+    <body>
+        <?php
+            echo "<p>Hello World!</p>";
+            //https://www.codeproject.com/Articles/759094/Step-by-Step-PHP-Tutorials-for-Beginners-Creating
+        ?>
+        <a href="login.php"> Click here to login 
+        <a href="register.php"> Click here to register 
+    </body>
+</html> 
