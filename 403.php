6f5e141d6e8f32c94446f30946443790734f1596
fernyoveja23 <ferninand_g@hotmail.com>
2018-12-04 23:43:55 -0600

    Controller y facade de usuario y protegida la carpeta de la app
    Se agregaron los archivos necesarios para poder proteger toda la
    carpeta donde se guardara los archivos de la app.

---
+++ 403.php
@@ -0,0 +1,17 @@
+</<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php echo "Acceso denegado"; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+    <body>
+        <div>
+            <p><?php echo "No es posible encontrar la pagina que buscas" ?></p>
+            <a href="http://localhost/yehoshua">Regresar a la pagina principal</a>
+        </div>
+    </body>
+</html>
