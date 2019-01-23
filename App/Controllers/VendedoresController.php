6926ecff817ebefa95f528d4d8c07f5b081c6e58
fernyoveja23 <ferninand_g@hotmail.com>
2019-01-22 18:14:38 -0600

    Arreglo de select

--- App/Controllers/VendedoresController.php
+++ App/Controllers/VendedoresController.php
@@ -162,7 +162,7 @@ class VendedoresController
         `lugar`.`DetalleLugar`,
         `imagen`.`NombreArchivo`, `imagen`.`Tipo`, `imagen`.`Imagen`
         FROM `venta` 
-        INNER JOIN `eventoturistico` ON `eventoturistico`. = `venta`.`idEvento`
+        INNER JOIN `eventoturistico` ON `eventoturistico`.`idEvento` = `venta`.`idEvento`
         INNER JOIN `vendedor` ON `vendedor`.`idVendedor` = `venta`.`idVendedor`
         INNER JOIN `lugar` ON `lugar`.`idLugar` = (SELECT `eventolugar`.`lugar_idLugar` FROM `eventolugar` 
         WHERE `eventolugar`.`eventoturistico_idEvento` = `eventoturistico`.`idEvento`)
